4a5c93574871eb2015c15cad58437dda62b4f839
Rihan Afrizal <rihanafrizal59@gmail.com>
2025-07-15 15:51:44 +0700

    Progres hari selasa 15 Juli 25

--- resources/views/edit-post.blade.php
+++ resources/views/edit-post.blade.php
@@ -1,15 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-
 <body>
-    <h1>hai garnen</h1>
+    <h1>Edit post disini</h1>
+    <a href="posts">Kembali ke post</a>
 </body>
-
 </html>
---
+++ resources/views/post.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <table style="width: 100%;" border="1">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Judul</th>
+                <th>Isi</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Judul 1</td>
+                <td>Isi 1</td>
+                <td>
+                    <a href="edit-post">Edit</a>
+                    <a href="#">Detail</a>
+                    <a href="#">Delete</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <a href="tambah-post">Tambah Post</a>
+</body>
+
+</html>
